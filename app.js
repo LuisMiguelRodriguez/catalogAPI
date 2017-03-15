33a370750008bfb362c39c8497a985e130cbcc77
LuisMiguelRodriguez <tanukitek@gmail.com>
2017-03-15 11:36:46 -0400

    DELETE function added

--- app.js
+++ app.js
@@ -50,5 +50,25 @@ app.post('/products', function(req, res){
   });
 });
 
+app.put('/products/:id', function(req, res){
+  db.products.findAndModify({
+  query:{_id: mongojs.ObjectId(req.params.id)},
+  update:{$set:{
+    name: req.body.name,
+    category: req.body.category,
+    description: req.body.description
+  }},
+  new: true
+} ,function(err, doc){
+    if(err){
+      console.log("HELLO WE HAVE A PROBLEM");
+      res.send(err);
+    } else {
+      console.log('Updating Product....');
+      res.json(doc);
+    }
+  });
+});
+
 app.listen(3000);
 console.log("Server is Running on port 3000");
