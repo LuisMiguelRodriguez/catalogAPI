87d5499891e707826d6e6578abd38c15fa5959e8
LuisMiguelRodriguez <tanukitek@gmail.com>
2017-03-15 07:43:22 -0400

    GET Request for All Products

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
 var express = require('express');
 var app = express();
+var mongojs = require('mongojs');
+var mongoose = require('mongoose');
+
+// Create Database Object Needs name Of Database
+// and name of collection/collections
+var db = mongojs('mongodb://localhost/catalog',['products']);
 
 app.get('/', function(req, res){
   res.send('It Works');
+});
+
+app.get('/products', function(req, res){
+    console.log('Fetching Products...');
+    db.products.find(function(err, docs){
+      if(err) {
+        res.send(err);
+      } else {
+        console.log('Sending Products...');
+        res.json(docs);
+      }
+    });
 });
 
 app.listen(3000);
